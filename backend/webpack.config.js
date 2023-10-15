fa2f076bdfaedcf97bcb713dbd9bf5d31065e61b
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-16 00:46:19 +0100

    ~Added SWC (Speedy Web Compiler) as a replacement to TSC (Typescript Compiler) due to it's great efficiency and performance
    Configured Webpack HMR (Hot Module Replacement) to utilize SWC (Speedy Web Compiler) instead of TSC (Typescript Compiler)

---
+++ backend/webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+const swcDefaultConfig = require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory().swcOptions;
+
+
+module.exports = {
+	entry: ['webpack/hot/poll?100', './src/main.ts'],
+	target: 'node',
+	externals: [
+		nodeExternals({
+			allowlist: ['webpack/hot/poll?100'],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /.tsx?$/,
+				use: {
+					loader: 'swc-loader',
+					options: swcDefaultConfig
+				},
+				exclude: /node_modules/,
+			},
+		],
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new RunScriptWebpackPlugin({ name: 'server.js', autoRestart: false }),
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'server.js',
+	},
+};
