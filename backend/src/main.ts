e09ee6cc445681ac7904a7840ab1a3f18b9d011c
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-23 21:29:32 +0100

    ~Added and configured Swagger
    Added a global prefix for version control

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 declare const module: any;
 
@@ -9,6 +10,15 @@ async function bootstrap() {
 
 	app.setGlobalPrefix("api/v1");
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+	const config = new DocumentBuilder()
+		.setTitle("ft_transcendence")
+		.setDescription("The official ft_transcendence API documentation.")
+		.setVersion("1.0")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("doc", app, document);
 
 	await app.listen(3000);
 
