75978a522e3299e247b014269be826f5c0d63ec7
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-23 21:21:40 +0100

    ~Created and configured the Prisma Service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "./prisma/prisma.module";
 
-@Module({})
+@Module({
+	imports: [PrismaModule],
+})
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 declare const module: any;
 
