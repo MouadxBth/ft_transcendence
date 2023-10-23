b6155f51782f2a677bcfe9a86268c55618c14531
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-23 21:25:30 +0100

    ~Added and configured a global Validation Pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 declare const module: any;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix("api/v1");
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
 	await app.listen(3000);
 
 	if (module.hot) {
