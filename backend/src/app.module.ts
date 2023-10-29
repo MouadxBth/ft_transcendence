9060e89f968209bb48a5f56646ec540b3d89c4c4
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-30 00:57:08 +0100

    ~Registered the ConfigModule
    Configured the ConfigModule to be used globally as well as to use the Config Validation Schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { UserModule } from "./user/user.module";
+import { ConfigModule } from "@nestjs/config";
+import { ConfigSchema } from "./config/config.schema";
 
 @Module({
-	imports: [PrismaModule, UserModule],
+	imports: [
+		PrismaModule,
+		UserModule,
+		ConfigModule.forRoot({
+			cache: true,
+			isGlobal: true,
+			validationSchema: ConfigSchema,
+		}),
+	],
 })
 export class AppModule {}
