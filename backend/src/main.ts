b8088437db48f97193ca8012e8e7ed0436afb3bd
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-30 16:08:11 +0100

    ~Modified main.ts
    Cleaned up the bootstrap function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 declare const module: any;
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-
+function configureApp(app: INestApplication<any>) {
 	app.setGlobalPrefix("api/v1");
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+}
 
+function configureSwagger(app: INestApplication<any>) {
 	const config = new DocumentBuilder()
 		.setTitle("ft_transcendence")
 		.setDescription("The official ft_transcendence API documentation.")
@@ -19,12 +19,22 @@ async function bootstrap() {
 
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("doc", app, document);
+}
 
-	await app.listen(3000);
-
+function hotModuleReplacement(app: INestApplication<any>) {
 	if (module.hot) {
 		module.hot.accept();
 		module.hot.dispose(() => app.close());
 	}
+}
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	configureApp(app);
+	configureSwagger(app);
+	hotModuleReplacement(app);
+
+	await app.listen(3000);
 }
 bootstrap();
