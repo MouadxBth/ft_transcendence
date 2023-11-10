9f26a5825ae55d19a0bf4b62f050149ef1b87753
broken_cookie <66413449+brkncookie@users.noreply.github.com>
2023-11-10 18:33:50 +0000

    Modified channel.controller.ts
    made the routes asynchronous
    
    elimintaed the password from being returned for security reasons

--- backend/src/channel/channel.controller.ts
+++ backend/src/channel/channel.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ChannelService } from './channel.service';
-import { CreateChannelDto } from './dto/create-channel.dto';
-import { UpdateChannelDto } from './dto/update-channel.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { ChannelService } from "./channel.service";
+import { CreateChannelDto } from "./dto/create-channel.dto";
+import { UpdateChannelDto } from "./dto/update-channel.dto";
 
-@Controller('channel')
+@Controller("channel")
 export class ChannelController {
-  constructor(private readonly channelService: ChannelService) {}
+	constructor(private readonly channelService: ChannelService) {}
 
-  @Post()
-  create(@Body() createChannelDto: CreateChannelDto) {
-    return this.channelService.create(createChannelDto).then(({password, ...channel}) => (channel));
-  }
+	@Post()
+	async create(@Body() createChannelDto: CreateChannelDto) {
+		const { password, ...channel } = await this.channelService.create(createChannelDto);
+		return channel;
+	}
 
-  @Get()
-  async findAll() {
-    return (await this.channelService.findAll()).map(({password, ...channel}) => (channel));
-  }
+	@Get()
+	async findAll() {
+		return (await this.channelService.findAll()).map(({ password, ...channel }) => channel);
+	}
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.channelService.findOne(id).then(({password, ...channel}) => (channel));
-  }
+	@Get(":id")
+	async findOne(@Param("id") id: string) {
+		const { password, ...channel } = await this.channelService.findOne(id);
+		return channel;
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChannelDto: UpdateChannelDto) {
-    return this.channelService.update(id, updateChannelDto).then(({password, ...channel}) => (channel));
-  }
+	@Patch(":id")
+	async update(@Param("id") id: string, @Body() updateChannelDto: UpdateChannelDto) {
+		const { password, ...channel } = await this.channelService.update(id, updateChannelDto);
+		return channel;
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.channelService.remove(id).then(({password, ...channel}) => (channel));
-  }
+	@Delete(":id")
+	async remove(@Param("id") id: string) {
+		const { password, ...channel } = await this.channelService.remove(id);
+		return channel;
+	}
 }
