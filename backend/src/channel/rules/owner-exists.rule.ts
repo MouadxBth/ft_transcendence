1c16bc1b71fd60bd64e9fe7b77b91642430a2be2
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-11-12 15:55:16 +0100

    ~Modified owner-exists.rule.ts
    Made the custom rule Injectable to allow dependency injection
    
    Added a guard clause to verify the ownerId value
    
    Added a default message for clarification purposes

--- backend/src/channel/rules/owner-exists.rule.ts
+++ backend/src/channel/rules/owner-exists.rule.ts
@@ -1,17 +1,28 @@
-import { ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+import { Injectable } from "@nestjs/common";
+import {
+	ValidationArguments,
+	ValidatorConstraint,
+	ValidatorConstraintInterface,
+} from "class-validator";
 import { PrismaService } from "src/prisma/prisma.service";
 
+@Injectable()
 @ValidatorConstraint({ name: "OwnerExistsRule", async: true })
 export class OwnerExistsRule implements ValidatorConstraintInterface {
 	constructor(private readonly prisma: PrismaService) {}
 
-	async validate(ownerId: string) {
+	async validate(ownerId: string | undefined) {
+		if (!ownerId) return false;
 		const owner = await this.prisma.user.findUnique({
 			where: { username: ownerId },
 		});
 
-		if (owner) return true;
+		if (!owner) return false;
 
-		return false;
+		return true;
+	}
+
+	defaultMessage(_args: ValidationArguments) {
+		return "Owner does not exist";
 	}
 }
