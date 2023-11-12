6e30906831db8a26d8ce57b397b3e8bb92fe4639
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-11-12 15:57:25 +0100

    ~Modified channel.module.ts
    Added OwnerExistsRule to the providers array to allow dependency injection

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ChannelService } from "./channel.service";
 import { ChannelController } from "./channel.controller";
+import { OwnerExistsRule } from "./rules/owner-exists.rule";
 
 @Module({
 	controllers: [ChannelController],
-	providers: [ChannelService],
+	providers: [ChannelService, OwnerExistsRule],
 })
 export class ChannelModule {}
