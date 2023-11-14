a006a6f9ecdf1e72a5931c6fa0e566b497a8b1e0
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-11-14 22:26:19 +0100

    ~Modified update-user.dto.ts
    Added twoFactorAuthenticationEnabled property to UpdateUserDto with proper documentation

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from "@nestjs/swagger";
+import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { CreateUserDto } from "./create-user.dto";
+import { IsBoolean, IsOptional } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+	@IsBoolean()
+	@IsOptional()
+	@ApiProperty({
+		description: "Used to enable/disable 2FA (Two Factor Authentication), by default, it's false",
+		example: true,
+	})
+	twoFactorAuthenticationEnabled?: boolean;
+}
