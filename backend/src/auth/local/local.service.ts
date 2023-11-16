b499bdd2f58e817837d7a50465c69c622f3e7950
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-11-16 02:38:47 +0100

    ~Added local.service.ts
    Added LocalService containing a validate method to verify a user's credentials and whether he's eligible to be authenticated or not
    
    Added a register method to register a new user with a given username and password which is hashed then stored
    
    Added a hash method to hash a given password using bcrypt

---
+++ backend/src/auth/local/local.service.ts
@@ -0,0 +1,39 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { UserService } from "src/user/user.service";
+import { CreateUserDto } from "src/user/dto/create-user.dto";
+import * as bcrypt from "bcrypt";
+
+@Injectable()
+export class LocalService {
+	constructor(private readonly userService: UserService) {}
+
+	async validate(username: string, pass: string) {
+		const user = await this.userService.findOne(username);
+
+		const { password, twoFactorAuthenticationSecret, ...result } = user;
+
+		if (!password) throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+
+		const comparison = await bcrypt.compare(pass, password);
+
+		if (!comparison) {
+			throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+		}
+
+		return result;
+	}
+
+	async register(dto: CreateUserDto) {
+		if (!dto.password)
+			throw new HttpException(
+				"Invalid credentials, a password is required!",
+				HttpStatus.BAD_REQUEST
+			);
+		dto.password = await this.hash(dto.password, 10);
+		return await this.userService.create(dto);
+	}
+
+	async hash(password: string, rounds: number) {
+		return bcrypt.hash(password, rounds);
+	}
+}
