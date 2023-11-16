2851c3155b5a648cd0f4611a7a2aa05bb42f8a77
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-11-16 22:22:36 +0100

    ~Modified auth.service.ts
    Renamed invalidateSession to logout
    
    Using a null-assertion operator for the config session name
    
    Removed unnecessary call to request.session.destroy since request.logOut handles this out of the box

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -7,23 +7,18 @@ import type { Request, Response } from "express";
 export class AuthService {
 	constructor(private readonly configService: ConfigService) {}
 
-	invalidateSession(request: Request, response: Response) {
+	logout(request: Request, response: Response) {
 		if (!request.isAuthenticated()) {
 			throw new HttpException("You are not logged in!", HttpStatus.BAD_REQUEST);
 		}
 
-		const sessionName = this.configService.get<string>("SESSION_NAME") || "connect.sid";
+		const sessionName = this.configService.get<string>("SESSION_NAME")!;
 
 		response.clearCookie(sessionName);
 
 		request.logOut({ keepSessionInfo: false }, (logOutError: unknown) => {
 			if (logOutError)
 				throw new HttpException("Failed to log out", HttpStatus.INTERNAL_SERVER_ERROR);
-
-			request.session.destroy((sessionError: unknown) => {
-				if (sessionError)
-					throw new HttpException("Failed to destroy session", HttpStatus.INTERNAL_SERVER_ERROR);
-			});
 		});
 
 		return "Logged out";
