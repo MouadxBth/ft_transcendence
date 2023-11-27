f908c522121ccbde0eb1826010ae897519bc11b7
broken_cookie <66413449+brkncookie@users.noreply.github.com>
2023-11-27 09:26:52 +0000

    Modified channel.module.ts accroding to the changes in the controller

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -1,8 +1,10 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { ChannelService } from "./channel.service";
 import { ChannelController } from "./channel.controller";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
+	imports: [forwardRef(() => AuthModule)],
 	controllers: [ChannelController],
 	providers: [ChannelService],
 })
