ddec70f8ec7b6183d729c156f4cf8508290ee2b4
broken_cookie <66413449+brkncookie@users.noreply.github.com>
2023-12-03 13:24:38 +0000

    Added channel-message.service.ts and message.dto.ts
    To handle the business logic needed by channel websocket gateway

---
+++ backend/src/channel/dto/message.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+
+
+
+export class MessageDto {
+    @IsString()
+    @IsNotEmpty()
+    message: string;
+
+    @IsString()
+    @IsNotEmpty()
+    channelName: string;
+
+}
