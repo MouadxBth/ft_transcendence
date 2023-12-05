9d5af8967b3dbf347316c7c5f23caabaeb1b8e62
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-12-05 07:38:34 +0100

    ~Modified blocked.module.ts
    Configured BlockedModule to use TwoFactorMiddleware for 2FA verificaition

--- backend/src/blocked/blocked.module.ts
+++ backend/src/blocked/blocked.module.ts
@@ -1,8 +1,9 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { BlockedService } from "./blocked.service";
 import { BlockedController } from "./blocked.controller";
 import { BlockedCache } from "./blocked.cache";
 import { UserModule } from "src/user/user.module";
+import { TwoFactorMiddleware } from "src/auth/two-factor/middleware/two-factor.middleware";
 
 @Module({
 	imports: [UserModule],
@@ -10,4 +11,8 @@ import { UserModule } from "src/user/user.module";
 	providers: [BlockedService, BlockedCache],
 	exports: [BlockedService],
 })
-export class BlockedModule {}
+export class BlockedModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(TwoFactorMiddleware).forRoutes(BlockedController);
+	}
+}
