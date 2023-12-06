d35389ff28523a40009efe111219f38cc3ec5493
broken_cookie <66413449+brkncookie@users.noreply.github.com>
2023-12-06 14:51:22 +0000

    Changed location of channel-owner.controller.ts, channel-owner.service.ts

---
+++ backend/src/channel/owner/channel-owner.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Post, Req, UseGuards } from "@nestjs/common";
+import { AuthenticatedGuard } from "src/auth/guards/authenticated.guard";
+import { ChannelOwnerService } from "./channel-owner.service";
+import type { Request } from "express";
+import { AdminOperationsDto } from "../dto/admin-operations.dto";
+import { User } from "@prisma/client";
+import { AuthenticatedUser } from "src/auth/entities/authenticated-user.entity";
+
+
+@Controller("channel")
+@UseGuards(AuthenticatedGuard)
+export class ChannelOwnerController
+{
+    constructor(private readonly channelOwnerService: ChannelOwnerService){}
+
+    @Post("admin/add")
+    async createAdmin(@Req() request: Request, @Body() dto: AdminOperationsDto)
+    {
+        const authenticatedUser = request.user as AuthenticatedUser;
+        return await this.channelOwnerService.createAdmin(authenticatedUser.user, dto);
+    }
+
+    @Delete("admin/delete")
+    async removeAdmin(@Req() request: Request, @Body() dto: AdminOperationsDto)
+    {
+        const authenticatedUser = request.user as AuthenticatedUser;
+        return await this.channelOwnerService.removeAdmin(authenticatedUser.user, dto);
+    }
+}
