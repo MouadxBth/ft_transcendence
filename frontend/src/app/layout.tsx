fd5ae1ed43e2fbf60436b5338b733c15b50077ee
Ihssane Koukou <ikoukou@e2r13p1.1337.ma>
2023-12-14 17:25:14 +0100

    init frontend branch

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Audiowide } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const audiowide = Audiowide({ subsets: ['latin'] , weight: ['400']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={audiowide.className}>{children}</body>
     </html>
   )
 }
