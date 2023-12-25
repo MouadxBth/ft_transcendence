7a471e0de57ecbb77f61b1217279c107f768b3ab
zu-23 <h.zakaria52@gmail.com>
2023-12-25 15:47:15 +0100

    sir t9wd

---
+++ game/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Pong from './Game';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Pong />
+      </header>
+    </div>
+  );
+}
+
+export default App;
