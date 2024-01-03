9b1660c9afc2bd7f60baea4516b84e09ec069415
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-03 12:55:58 +0100

    Modified addChannel

--- frontend/src/components/addChannel.tsx
+++ frontend/src/components/addChannel.tsx
@@ -1,34 +1,106 @@
+import { userContext } from "@/app/chat/userContext";
 import { loggedUser } from "@/app/chat/userData";
-import { Plus } from "lucide-react";
-import { useState } from "react";
+import { Plus, X } from "lucide-react";
+import { useContext, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from "./ui/button";
+  
 
 function AddChannel() {
-    const [newlogged, setUser] = useState(loggedUser);
+	const {userData, setUserData} = useContext(userContext);
+	const closeBtnRef = useRef(null);
+	const {reset, handleSubmit, register}= useForm<{username: string}>({
+		defaultValues: {
+			username: ""
+		},
+	})
+
+	function onSubmit(value: {username: string}, e: any) {
+		e.preventDefault();
+		if (value.username.trim() && value.username.trim() !== '') {
+			const userExists = userData.conversations.some(
+				conversation => conversation.user === value.username.trim()
+			);
+		if (!userExists)
+		{
+			userData.channels.push({user: value.username.trim(), data: []})
+			setUserData({...userData})
+			closeBtnRef.current.click();
+		}
+	}
+	reset({
+		username: ""
+	})
+}
 	
-		const handleAddUserClick = () => {
-			const newUserName = prompt('Enter the name of the new channel:');
-			if (newUserName) {
-				loggedUser.channels.push({user: newUserName, data: []})
-				setUser(prevUser => ({
-					...prevUser,
-					channels: [
-					  ...prevUser.channels,
-					  { user: newUserName, data: [] },
-					],
-				  }));
-			}
-		  };
    return(
-	<>
-	<div>
-		<button className=" rounded-full h-10 w-10 bg-white text-black absolute top-[93%] right-[81%]" onClick={handleAddUserClick}>
-			<div className="flex justify-center">
-				<Plus/>
-			</div>
-		</button>
-	</div>
-</>
+    <>
+        <div>
+			<Dialog>
+				<DialogTrigger className=" rounded-full h-10 w-10 bg-white text-black absolute top-[93%] right-[81%]">
+					<div className="flex justify-center">
+						<Plus/>
+					</div>
+				</DialogTrigger>
+				<DialogContent className="flex flex-col text-white">
+					<div className="flex justify-between items-center">
+						<DialogTitle >Enter channel name</DialogTitle>
+						<DialogClose ref={closeBtnRef} className="text-white">
+							<X/>
+						</DialogClose>
+					</div>
+						<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="flex flex-col justify-between items-center">
+							<div>
+								<input className="bg-black w-96 h-12 border rounded-full px-2" placeholder="username" type="text" {...register("username")}/>
+							</div>
+							<div>
+								<Button variant="ghost" className="mt-4 hover:bg-white hover:text-black border rounded-xl w-20 h-6 text-white" type="submit">
+										Done
+									</Button>
+							</div>
+					</div>
+					</form>
+				</DialogContent>
+			</Dialog>
+        </div>
+    </>
    )
+//     const [newlogged, setUser] = useState(loggedUser);
+	
+// 		const handleAddUserClick = () => {
+// 			const newUserName = prompt('Enter the name of the new channel:');
+// 			if (newUserName) {
+// 				loggedUser.channels.push({user: newUserName, data: []})
+// 				setUser(prevUser => ({
+// 					...prevUser,
+// 					channels: [
+// 					  ...prevUser.channels,
+// 					  { user: newUserName, data: [] },
+// 					],
+// 				  }));
+// 			}
+// 		  };
+//    return(
+// 	<>
+// 	<div>
+// 		<button className=" rounded-full h-10 w-10 bg-white text-black absolute top-[93%] right-[81%]" onClick={handleAddUserClick}>
+// 			<div className="flex justify-center">
+// 				<Plus/>
+// 			</div>
+// 		</button>
+// 	</div>
+// </>
+//    )
   }
   
   export default AddChannel
