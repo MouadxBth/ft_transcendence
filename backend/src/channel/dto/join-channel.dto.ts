c7b59690b3ac9fea8b65a339ad373a988ffd88a9
broken_cookie <66413449+brkncookie@users.noreply.github.com>
2024-01-11 11:59:02 +0000

    Modified admin-operations.dto.ts, join-channel.dto.ts
    to document the DTOs

--- backend/src/channel/dto/join-channel.dto.ts
+++ backend/src/channel/dto/join-channel.dto.ts
@@ -1,13 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-
-
 export class JoinChannelDto {
-    @IsString()
-    @IsNotEmpty()
-    channel: string;
+	@IsString()
+	@IsNotEmpty()
+	@ApiProperty({
+		description: "Name of the channel",
+		example: "general_channel",
+	})
+	channel: string;
 
-    @IsString()
-    @IsOptional()
-    password?: string | null;
-} 
+	@IsString()
+	@IsOptional()
+	@ApiProperty({
+		description: "Password for the channel (optional)",
+		example: "secretpassword",
+		required: false,
+	})
+	password?: string | null;
+}
