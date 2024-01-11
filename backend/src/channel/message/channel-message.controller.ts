08147f7be3bc33ddc584e8217a370d22edaebc60
broken_cookie <66413449+brkncookie@users.noreply.github.com>
2024-01-11 11:57:23 +0000

    Modified channel-message.controller.ts
    to document the controller

--- backend/src/channel/message/channel-message.controller.ts
+++ backend/src/channel/message/channel-message.controller.ts
@@ -15,13 +15,21 @@ import type { Request } from "express";
 import { AuthenticatedGuard } from "src/auth/guards/authenticated.guard";
 import { ChannelMessageService } from "./channel-message.service";
 import { AuthenticatedUser } from "src/auth/entities/authenticated-user.entity";
+import { ApiBody, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("channel")
 @Controller("channel")
 @UseGuards(AuthenticatedGuard)
 export class ChannelMessageController {
 	constructor(private readonly channelMessageService: ChannelMessageService) {}
 
 	@Get(":channel/messages")
+	@ApiOperation({ summary: "Get messages from a channel" })
+	@ApiParam({ name: "channel", description: "Channel name" })
+	@ApiQuery({ name: "cursor", description: "Cursor for pagination", required: false })
+	@ApiQuery({ name: "quantity", description: "Quantity for pagination", required: false })
+	@ApiResponse({ status: 200, description: "Successfully retrieved channel messages" })
+	@ApiResponse({ status: 400, description: "Bad Request - Invalid cursor or quantity" })
 	async findMany(
 		@Param("channel") channel: string,
 		@Query("cursor") cursor: string | undefined,
@@ -43,6 +51,19 @@ export class ChannelMessageController {
 		return await this.channelMessageService.findAll(channel);
 	}
 	@Patch(":channel/messages/:id")
+	@ApiOperation({ summary: "Update a channel message" })
+	@ApiParam({ name: "channel", description: "Channel name" })
+	@ApiParam({ name: "id", description: "Message ID" })
+	@ApiResponse({ status: 200, description: "Successfully updated channel message" })
+	@ApiResponse({ status: 400, description: "Bad Request - Invalid ID or message is empty" })
+	@ApiResponse({
+		status: 403,
+		description:
+			"Forbidden - User is not the sender of the message or message does not belong to the channel",
+	})
+	@ApiBody({
+		description: "a message varibale of type string in json format",
+	})
 	async update(
 		@Req() request: Request,
 		@Param("channel") channel: string,
@@ -65,6 +86,16 @@ export class ChannelMessageController {
 	}
 
 	@Delete(":channel/messages/:id")
+	@ApiOperation({ summary: "Delete a channel message" })
+	@ApiParam({ name: "channel", description: "Channel name" })
+	@ApiParam({ name: "id", description: "Message ID" })
+	@ApiResponse({ status: 200, description: "Successfully deleted channel message" })
+	@ApiResponse({ status: 400, description: "Bad Request - Invalid ID" })
+	@ApiResponse({
+		status: 403,
+		description:
+			"Forbidden - User is not the sender of the message or message does not belong to the channel",
+	})
 	async delete(
 		@Req() request: Request,
 		@Param("channel") channel: string,
