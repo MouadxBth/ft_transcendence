d0fb525d5cd12468a0cc41888538daf049afcb68
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-17 23:59:04 +0100

    ~Modified next.config.js
    Allowed a set of trusted sources to provide the frontend with images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "github.com",
+			},
+			{
+				protocol: "https",
+				hostname: "robohash.org",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
