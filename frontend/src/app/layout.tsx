1325b7b52e02130420bd0dc75125f64b7185af63
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-18 14:48:53 +0100

    ~Modified layout.tsx
    Modified RootLayout
    
    Added React Query Client Provider
    
    Added Authentication Context Provider
    
    Added Shadcn-UI Toaster

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,25 +1,35 @@
 import "./globals.css";
+import type { Metadata } from "next";
+import AuthenticationContextProvider from "@/providers/AuthenticationContextProvider";
 import { AUTHORS } from "@/lib/authors";
 import { Audiowide } from "next/font/google";
-import type { Metadata } from "next";
+import { ReactQueryClientProvider } from "@/providers/ReactQueryClientProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 const audiowide = Audiowide({ subsets: ["latin"], weight: "400" });
+
+const authors = AUTHORS.map((author) => {
+	return {
+		name: `${author.firstName} ${author.lastName}`,
+		url: author.socials[0].link,
+	};
+});
 
 export const metadata: Metadata = {
 	title: "ft_transcendence",
 	description: "Through my work, you shall transcend!",
-	authors: AUTHORS.map((author) => {
-		return {
-			name: `${author.firstName} ${author.lastName}`,
-			url: author.socials[0].link,
-		};
-	}),
+	authors: authors,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={audiowide.className}>{children}</body>
+			<body className={audiowide.className}>
+				<ReactQueryClientProvider>
+					<AuthenticationContextProvider>{children}</AuthenticationContextProvider>
+				</ReactQueryClientProvider>
+				<Toaster />
+			</body>
 		</html>
 	);
 }
