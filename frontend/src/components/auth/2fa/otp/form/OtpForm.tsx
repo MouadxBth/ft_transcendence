f03bbf8ff45132c4dc8281e38bbd054cbf1856aa
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-18 15:38:34 +0100

    ~Added OtpForm.tsx
    Added OtpFormField.tsx
    
    Added Otp Form to verify 2FA Otp

---
+++ frontend/src/components/auth/2fa/otp/form/OtpForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import FormButton from "@/components/auth/FormButton";
+import { OtpFormField } from "./OtpFormField";
+import { OtpFormType, otpFormSchema } from "@/lib/types/otp-form";
+import useOtp from "@/hooks/authentication/useOtp";
+
+export const OtpForm = () => {
+	const form = useForm<OtpFormType>({
+		resolver: zodResolver(otpFormSchema),
+		defaultValues: {
+			otp: "",
+		},
+	});
+
+	const otp = useOtp();
+
+	async function onSubmit(data: OtpFormType) {
+		otp.mutate(data);
+		form.reset();
+	}
+
+	return (
+		<Form {...form}>
+			<form
+				id="otp"
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="space-y-4"
+			>
+				<OtpFormField
+					control={form.control}
+					name="otp"
+					placeholder="Enter your 2FA OTP provided by your authentication app"
+					label="OTP"
+					type="text"
+				/>
+			</form>
+
+			<FormButton
+				isPending={otp.isPending}
+				form="otp"
+			>
+				Submit
+			</FormButton>
+		</Form>
+	);
+};
