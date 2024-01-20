d27a58197b77ec41af6f48f0c1f5e543a520bac3
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-19 21:41:14 -0500

    adding password and confirm password fields

--- frontend/src/components/settings/form.tsx
+++ frontend/src/components/settings/form.tsx
@@ -15,46 +15,27 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AuthenticationContext } from "@/contexts/AuthenticationContext";
+import { ScrollArea } from "../ui/scroll-area";
 
 const formSchema = z
 	.object({
 		username: z.string(),
-		firstname: z
-			.string()
-			.min(2, {
-				message: "First name must be at least 2 characters.",
-			})
-			.optional(),
-		nickname: z
-			.string()
-			.min(2, {
-				message: "Nickname must be at least 2 characters.",
-			})
-			.optional(),
-		lastname: z
-			.string()
-			.min(2, {
-				message: "First name must be at least 2 characters.",
-			})
-			.optional(),
-		password: z
-			.string()
-			.min(8, {
-				message: "Password must be at least 8 characters.",
-			})
-			.optional(),
+		firstname: z.string().min(2),
+		nickname: z.string().min(2),
+		lastname: z.string().min(2),
+		password: z.string().min(2),
 		confirmpassword: z.string(),
 	})
-	.refine((data) => data.password !== data.confirmpassword, {
+	.refine((data) => data.password === data.confirmpassword, {
 		message: "Passwords don't match",
-		path: ["confirmPassword"],
+		path: ["confirmpassword"],
 	});
 
 function ProfileForm() {
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			username: "",
+			username: "current username",
 			nickname: "current nickname",
 			firstname: "current first name",
 			lastname: "current last name",
@@ -71,75 +52,125 @@ function ProfileForm() {
 				onSubmit={form.handleSubmit(onSubmit)}
 				className="grid grid-cols-2 gap-2 w-96 space-y-4 p-2"
 			>
-				<FormField
-					control={form.control}
-					name="username"
-					render={() => (
-						<FormItem className="col-span-2">
-							<FormLabel>Username</FormLabel>
-							<FormControl>
-								<Input
-									disabled
-									placeholder="username"
-								/>
-							</FormControl>
-							<FormDescription className="text-xs">
-								you can&apos;t change your username
-							</FormDescription>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="nickname"
-					render={({ field }) => (
-						<FormItem className="col-span-2">
-							<FormLabel>Nickname</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="nickname"
-									{...field}
-								/>
-							</FormControl>
-							<FormDescription className="text-xs">this is you public display name</FormDescription>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="firstname"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>First Name</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="First Name"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="lastname"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Last Name</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="Last Name"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				{/* <Button type="submit">Submit</Button> */}
+				<ScrollArea className="h-80 col-span-2">
+					<div className="grid grid-cols-2 gap-2 p-4">
+						<FormField
+							control={form.control}
+							name="username"
+							render={({ field }) => (
+								<FormItem className="col-span-2">
+									<FormLabel>Username</FormLabel>
+									<FormControl>
+										<Input
+											disabled
+											placeholder="username"
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription className="text-xs">
+										you can&apos;t change your username
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="nickname"
+							render={({ field }) => (
+								<FormItem className="col-span-2">
+									<FormLabel>Nickname</FormLabel>
+									<FormControl>
+										<Input
+											placeholder="nickname"
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription className="text-xs">
+										this is you public display name
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="firstname"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>First Name</FormLabel>
+									<FormControl>
+										<Input
+											placeholder="First Name"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="lastname"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Last Name</FormLabel>
+									<FormControl>
+										<Input
+											placeholder="Last Name"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input
+											type="password"
+											placeholder="Password"
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription className="text-xs">
+										here you can set a new password
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="confirmpassword"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Confirm Password</FormLabel>
+									<FormControl>
+										<Input
+											type="password"
+											placeholder="Password confirmation"
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription className="text-xs">confirm your new password</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+				</ScrollArea>
+				<Button
+					className="col-span-2"
+					type="submit"
+				>
+					Submit
+				</Button>
 			</form>
 		</Form>
 	);
