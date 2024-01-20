2e637cf2e8ec6e5e9d85cb760d8e0df6e0699080
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-20 22:50:05 +0100

    adding sidebar in progress

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -16,6 +16,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 import CustomAvatar from "@/components/settings/CustomAvatar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Settings() {
 	return (
@@ -26,10 +27,51 @@ function Settings() {
 					<p className="text-muted-foreground">Manage your account.</p>
 				</div>
 				<Separator className="shrink-0 h-[1px] w-full my-6" />
-				<div className="flex flex-col justify-center items-center">
+				<div className="flex">
+					<aside className="w-96 p-12 h-fit">
+						<div className="space-y-4">
+							<div>profile</div>
+							<div>2FA</div>
+						</div>
+					</aside>
+					<div className="w-full pl-12">
+						<CustomAvatar />
+						<ProfileForm />
+					</div>
+				</div>
+				{/* <Tabs className="space-y-24 flex">
+					<TabsList className="flex flex-col h-fit bg-white/0 w-96 space-y-3">
+						<TabsTrigger
+							value="profile"
+							className="w-full"
+						>
+							Profile
+						</TabsTrigger>
+						<TabsTrigger
+							value="2fa"
+							className="w-full"
+						>
+							2FA
+						</TabsTrigger>
+					</TabsList>
+					<TabsContent
+						value="profile"
+						className="flex flex-col justify-center items-start  w-full bg-red-700"
+					>
+						<CustomAvatar />
+						<ProfileForm />
+					</TabsContent>
+					<TabsContent
+						value="2fa"
+						className="flex flex-col justify-center items-start"
+					>
+						2FA settings
+					</TabsContent>
+				</Tabs> */}
+				{/* <div className="flex flex-col justify-center items-center">
 					<CustomAvatar />
 					<ProfileForm />
-				</div>
+				</div> */}
 			</ScrollArea>
 		</main>
 	);
