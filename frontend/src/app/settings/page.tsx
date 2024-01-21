32d8db3c3258f1186cce4c1b019d038e950fc646
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-21 10:47:14 +0100

    used Tabs shadcn componenet to switch between profile settings and 2FA settings

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,47 +1,45 @@
 "use client";
-import React, { useState } from "react";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
 import ProfileForm from "@/components/settings/form";
-import Navbar from "@/components/navbar/Navbar";
 import { Separator } from "@/components/ui/separator";
-import { useAuthentication } from "@/hooks/authentication/useAuthentication";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Button } from "@/components/ui/button";
 import CustomAvatar from "@/components/settings/CustomAvatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Settings() {
 	return (
 		<main>
-			<ScrollArea className="h-screen p-10">
+			<ScrollArea className="max-h-screen p-8">
 				<div>
 					<h2 className="text-2xl tracking-tight">Settings</h2>
 					<p className="text-muted-foreground">Manage your account.</p>
 				</div>
 				<Separator className="shrink-0 h-[1px] w-full my-6" />
-				<div className="flex">
-					<aside className="w-96 p-12 h-fit">
-						<div className="space-y-4">
-							<div>profile</div>
-							<div>2FA</div>
+				<Tabs
+					defaultValue="profile"
+					className="flex flex-row h-screen w-full"
+				>
+					<TabsList className="flex flex-col h-full justify-start items-start space-y-3 bg-hidden w-64">
+						<TabsTrigger value="profile">Profile</TabsTrigger>
+						<TabsTrigger value="2fa">2FA</TabsTrigger>
+					</TabsList>
+					<TabsContent
+						value="profile"
+						className=" w-full pl-12 border-l"
+					>
+						<div className="flex flex-col justify-center space-y-6">
+							<CustomAvatar />
+							<ProfileForm />
 						</div>
-					</aside>
-					<div className="w-full pl-12">
-						<CustomAvatar />
-						<ProfileForm />
-					</div>
-				</div>
+					</TabsContent>
+					<TabsContent
+						value="2fa"
+						className="w-full pl-12 border-l"
+					>
+						2FA
+					</TabsContent>
+				</Tabs>
 			</ScrollArea>
 		</main>
 	);
 }
-
 export default Settings;
