3567e9ad4cbfd68a958eaebbc7e86cc67f6d7550
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-21 21:14:28 +0100

    added SettingsTabs component

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -4,6 +4,7 @@ import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import CustomAvatar from "@/components/settings/CustomAvatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import SettingsTabs from "@/components/settings/settingsTabs";
 
 function Settings() {
 	return (
@@ -14,30 +15,7 @@ function Settings() {
 					<p className="text-muted-foreground">Manage your account.</p>
 				</div>
 				<Separator className="shrink-0 h-[1px] w-full my-6" />
-				<Tabs
-					defaultValue="profile"
-					className="flex flex-row h-screen w-full"
-				>
-					<TabsList className="flex flex-col h-full justify-start items-start space-y-3 bg-hidden w-64">
-						<TabsTrigger value="profile">Profile</TabsTrigger>
-						<TabsTrigger value="2fa">2FA</TabsTrigger>
-					</TabsList>
-					<TabsContent
-						value="profile"
-						className=" w-full pl-12 border-l"
-					>
-						<div className="flex flex-col justify-center space-y-6">
-							<CustomAvatar />
-							<ProfileForm />
-						</div>
-					</TabsContent>
-					<TabsContent
-						value="2fa"
-						className="w-full pl-12 border-l"
-					>
-						2FA
-					</TabsContent>
-				</Tabs>
+				<SettingsTabs />
 			</ScrollArea>
 		</main>
 	);
