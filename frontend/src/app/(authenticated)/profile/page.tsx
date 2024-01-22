98f8bab235f0f1bd87b0ff8a7b3df2c22e8fa654
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-22 19:16:47 +0100

    ~Added page.tsx
    Added initial Profile Page

---
+++ frontend/src/app/(authenticated)/profile/page.tsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+	return (
+		<article className="h-full flex justify-center flex-col bg-red-500">
+			<div>PROFILE</div>
+		</article>
+	);
+};
+
+export default ProfilePage;
