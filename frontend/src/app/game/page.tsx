7fad1c73953b1e4e5ec8aab282f3c60245e8d41b
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-22 18:07:26 +0100

    ~Added page.tsx
    Added Game Page

---
+++ frontend/src/app/game/page.tsx
@@ -0,0 +1,11 @@
+import Pong from "@/components/game/Pong";
+
+const GamePage = () => {
+	return (
+		<main className="h-screen flex justify-center items-center">
+			<Pong />
+		</main>
+	);
+};
+
+export default GamePage;
