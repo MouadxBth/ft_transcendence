524f715160ba70f915ebbf2e57df0354f3c9218c
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-23 17:00:19 +0100

    ~Modified Profile Pages
    Removed deprecated profile pages and add new layout components

--- frontend/src/app/(authenticated)/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-const ProfilePage = () => {
-	return (
-		<article className="h-full flex justify-center flex-col bg-red-500">
-			<div>PROFILE</div>
-		</article>
-	);
-};
-
-export default ProfilePage;
---
+++ frontend/src/app/(sockets)/(authenticated)/profile/[id]/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import ProfileInfoCard from "@/components/profile/info/ProfileInfoCard";
+import ProfileInfoCardSkeleton from "@/components/profile/info/skeleton/ProfileInfoCardSkeleton";
+import useProfile from "@/hooks/user/useProfile";
+import { User } from "@/lib/types/user";
+import { notFound } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const ProfilePage = ({ params }: { params: { id: string } }) => {
+	const { data, isLoading } = useProfile(params.id);
+	const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		if (isLoading) return;
+
+		if (!data) return notFound();
+
+		setUser(data);
+		setLoading(false);
+	}, [isLoading, data]);
+
+	return (
+		<article className="h-full flex flex-col justify-center">
+			{loading ? <ProfileInfoCardSkeleton /> : <ProfileInfoCard user={user!} />}
+		</article>
+	);
+};
+
+export default ProfilePage;
