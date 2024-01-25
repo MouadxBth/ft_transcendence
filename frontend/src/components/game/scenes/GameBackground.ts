911091994f785663882f9869ce10ce8465ec508c
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-25 04:06:24 +0100

    ~Modified GameBackground.ts
    Replaced straight line with dashed line
    
    Co-authored-by: MouadxBth <mouad.bouthaich@outlook.com>
    Co-authored-by: Zu-23 <h.zakaria52@gmail.com>

--- frontend/src/components/game/scenes/GameBackground.ts
+++ frontend/src/components/game/scenes/GameBackground.ts
@@ -4,8 +4,15 @@ export class GameBackground extends Phaser.Scene {
 	preload() {}
 
 	create() {
-		this.add.line(400, 300, 0, 0, 0, 800, Colors.White, 1).setLineWidth(2.5, 2.5);
+		// this.add.line(400, 300, 0, 0, 0, 800, Colors.White, 1).setLineWidth(2.5, 2.5);
 
-		this.add.circle(400, 300, 50).setStrokeStyle(5, Colors.White, 1);
+		// this.add.circle(400, 300, 50).setStrokeStyle(5, Colors.White, 1);
+
+		for (let index = 0; index < 30; index++) {
+			if (index % 2 !== 0)
+				this.add
+					.line(400, 0, 0, index * 30, 0, (index + 1) * 30, Colors.White, 1)
+					.setLineWidth(6, 6);
+		}
 	}
 }
