848ae0f85fadc20540dcec006ae0757859bfd511
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-25 17:43:04 +0100

    changed page.tsx
    modified style of SettingsPage

--- frontend/src/app/(sockets)/(authenticated)/settings/page.tsx
+++ frontend/src/app/(sockets)/(authenticated)/settings/page.tsx
@@ -1,9 +1,13 @@
 "use client";
-import SettingsForm from "@/components/settings/SettingsForm";
-import TwoFaContent from "@/components/settings/TwoFaComponent";
+import TwoFaContent from "@/components/settings/2fa/TwoFaContent";
+import SettingsForm from "@/components/settings/form/SettingsForm";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-const SettingsHeader = ({ className }: { className?: string }) => {
+export interface SettingsHeaderProps {
+	className?: string;
+}
+
+const SettingsHeader = ({ className }: SettingsHeaderProps) => {
 	return (
 		<div className="pl-2 pb-2 space-y-1 w-56">
 			<h1 className="text-2xl font-semibold leading-none tracking-tight">Settings</h1>
@@ -14,17 +18,17 @@ const SettingsHeader = ({ className }: { className?: string }) => {
 
 const SettingsPage = () => {
 	return (
-		<main className="h-full flex items-center">
-			<article className="flex flex-col h-full justify-center space-y-6 w-full container">
-				<SettingsHeader className="place-content-start" />
-				<div className="flex space-x-2 items-center">
-					<ScrollArea className="2xl:h-full  h-96 rounded-xl w-full p-2 pt-0 border-2">
-						<SettingsForm />
-					</ScrollArea>
-					<TwoFaContent className="w-full h-full" />
-				</div>
-			</article>
-		</main>
+		<article className="flex items-satrt justify-center flex-col h-full w-[2000px] space-y-3 container">
+			<SettingsHeader />
+			<div className="flex justify-arround space-x-2 items-center max-md:flex-col max-md:space-y-2 w-full">
+				<ScrollArea className="2xl:h-full  h-[500px] rounded-xl w-full p-2 pt-0 border-2">
+					<SettingsForm />
+				</ScrollArea>
+				<ScrollArea className="2xl:h-full h-[500px] rounded-xl w-full p-2 pt-0 border-2">
+					<TwoFaContent />
+				</ScrollArea>
+			</div>
+		</article>
 	);
 };
 
