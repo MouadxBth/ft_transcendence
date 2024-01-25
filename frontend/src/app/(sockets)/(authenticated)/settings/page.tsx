19065b8c3a4f9e20fee5d44b6083bcde49355cf1
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-25 18:06:48 +0100

    Modified page.tsx
    Removed SettingsHeader and moved it to another file

--- frontend/src/app/(sockets)/(authenticated)/settings/page.tsx
+++ frontend/src/app/(sockets)/(authenticated)/settings/page.tsx
@@ -1,20 +1,8 @@
 "use client";
 import TwoFaContent from "@/components/settings/2fa/TwoFaContent";
+import SettingsHeader from "@/components/settings/SettingsHeader";
 import SettingsForm from "@/components/settings/form/SettingsForm";
 import { ScrollArea } from "@/components/ui/scroll-area";
-
-export interface SettingsHeaderProps {
-	className?: string;
-}
-
-const SettingsHeader = ({ className }: SettingsHeaderProps) => {
-	return (
-		<div className="pl-2 pb-2 space-y-1 w-56">
-			<h1 className="text-2xl font-semibold leading-none tracking-tight">Settings</h1>
-			<p className="text-sm text-muted-foreground">Manage Your Account</p>
-		</div>
-	);
-};
 
 const SettingsPage = () => {
 	return (
