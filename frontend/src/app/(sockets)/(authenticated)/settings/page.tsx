4d667cd52a14f9a8edc1f78c14539775955ab524
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-25 18:02:36 +0100

    Modified page.tsx
    Modified style of SettingsPage

--- frontend/src/app/(sockets)/(authenticated)/settings/page.tsx
+++ frontend/src/app/(sockets)/(authenticated)/settings/page.tsx
@@ -18,7 +18,7 @@ const SettingsHeader = ({ className }: SettingsHeaderProps) => {
 
 const SettingsPage = () => {
 	return (
-		<article className="flex items-satrt justify-center flex-col h-full w-[2000px] space-y-3 container">
+		<article className="flex flex-col h-full justify-center space-y-3 w-full container">
 			<SettingsHeader />
 			<div className="flex justify-arround space-x-2 items-center max-md:flex-col max-md:space-y-2 w-full">
 				<ScrollArea className="2xl:h-full  h-[500px] rounded-xl w-full p-2 pt-0 border-2">
