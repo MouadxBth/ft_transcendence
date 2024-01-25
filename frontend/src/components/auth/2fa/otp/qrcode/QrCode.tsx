e60ca8fa40bdcae2015d5ed3b99fab321431c94d
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-25 10:29:28 +0100

    added ImageReveerser to modify the qrcode style
    the qrcode is just looking good now with our website theme

--- frontend/src/components/auth/2fa/otp/qrcode/QrCode.tsx
+++ frontend/src/components/auth/2fa/otp/qrcode/QrCode.tsx
@@ -5,6 +5,7 @@ import useQrCode from "@/hooks/authentication/useQrCode";
 import Image from "next/image";
 import QrCodeError from "./QrCodeError";
 import { useAuthentication } from "@/hooks/authentication/useAuthentication";
+import ImageReverser from "../../../../../../ImageReverser";
 
 const QrCode = () => {
 	const { authenticatedUser } = useAuthentication();
@@ -14,15 +15,7 @@ const QrCode = () => {
 
 	if (isError) return <QrCodeError error={error} />;
 
-	return (
-		<Image
-			className="bg-white"
-			src={data!}
-			alt="qrcode"
-			width={200}
-			height={200}
-		/>
-	);
+	return <ImageReverser imageUrl={data!} />;
 };
 
 export default QrCode;
