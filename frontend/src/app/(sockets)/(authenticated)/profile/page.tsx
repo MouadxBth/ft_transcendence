0d7241774c973e7ba8f64554587dab99e176a031
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-26 07:33:24 +0100

    ~Modified page.tsx
    Added ProfileFriendsCard Component to ProfilePage

--- frontend/src/app/(sockets)/(authenticated)/profile/page.tsx
+++ frontend/src/app/(sockets)/(authenticated)/profile/page.tsx
@@ -1,9 +1,11 @@
+import ProfileFriendsCard from "@/components/profile/friends/ProfileFriendsCard";
 import ProfileInfoCard from "@/components/profile/info/ProfileInfoCard";
 
 const ProfilePage = () => {
 	return (
 		<article className="h-full flex flex-col">
 			<ProfileInfoCard />
+			<ProfileFriendsCard />
 		</article>
 	);
 };
