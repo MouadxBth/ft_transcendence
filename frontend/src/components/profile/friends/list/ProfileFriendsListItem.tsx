73e4759c5085e7b31391113e05da21bddf53b575
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-26 19:50:57 +0100

    Modified ProfileFriendsListItem.tsx
    Added ProfileOnlineStatus component
    
    it shows the status of users on the friends list

--- frontend/src/components/profile/friends/list/ProfileFriendsListItem.tsx
+++ frontend/src/components/profile/friends/list/ProfileFriendsListItem.tsx
@@ -3,6 +3,7 @@ import { Separator } from "@/components/ui/separator";
 import { FriendType } from "@/lib/types/friend";
 import { User } from "@/lib/types/user";
 import Link from "next/link";
+import ProfileOnlineStatus from "../../info/avatar/ProfileOnlineStatus";
 
 const ProfileFriendsListItem = ({
 	username,
@@ -19,24 +20,30 @@ const ProfileFriendsListItem = ({
 		<>
 			<Link
 				href={`/profile/${username}`}
-				className="flex space-x-2  w-full bg-black p-2 rounded-2xl"
+				className="flex space-x-2  w-full bg-black p-2 rounded-2xl justify-between items-center"
 			>
-				<Avatar className="h-14 w-14">
-					<AvatarImage
-						className="object-cover"
-						src={avatarSource}
-						alt={nickname!}
-					/>
-					<AvatarFallback>{nickname.toUpperCase().slice(0, 2)}</AvatarFallback>
-				</Avatar>
-				<div className="flex flex-col items-start">
-					<div className="text-md">{nickname}</div>
-					<div className="text-xs">@{username}</div>
-					<div className="text-xs flex space-x-2 text-muted-foreground">
-						<span>{firstName}</span>
-						<span>{lastName}</span>
+				<div className="flex items-center space-x-3">
+					<Avatar className="h-14 w-14">
+						<AvatarImage
+							className="object-cover"
+							src={avatarSource}
+							alt={nickname!}
+						/>
+						<AvatarFallback>{nickname.toUpperCase().slice(0, 2)}</AvatarFallback>
+					</Avatar>
+					<div className="flex flex-col items-start">
+						<div className="text-md">{nickname}</div>
+						<div className="text-xs">@{username}</div>
+						<div className="text-xs flex space-x-2 text-muted-foreground">
+							<span>{firstName}</span>
+							<span>{lastName}</span>
+						</div>
 					</div>
 				</div>
+				<ProfileOnlineStatus
+					username={username}
+					className="h-6 items-center w-14 flex justify-center"
+				/>
 			</Link>
 			<Separator className="my-1" />
 		</>
