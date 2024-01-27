ee195486b0d98ba6f30889a9e2525d54a7b4fc55
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-27 19:20:29 +0100

    Added GameMatchmaking.tsx
    Added GameMatchmaking component

---
+++ frontend/src/components/game/components/GameMatchmaking/GameMatchmaking.tsx
@@ -0,0 +1,67 @@
+"use client";
+import useFriends from "@/hooks/user/useFriends";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import React from "react";
+import { useEffect, useState } from "react";
+import { type CarouselApi } from "@/components/ui/carousel";
+import { useAuthentication } from "@/hooks/authentication/useAuthentication";
+import { TabsContent } from "@/components/ui/tabs";
+import SelectMode from "./SelectMode";
+import SelectModeColor from "./SelectModeColor";
+import GameMatchmakingButtons from "./GameMatchmakingButtons";
+import GameModeTabsTitle from "./MatchmakingTitle";
+import GameSelectFriend from "./GameSelectFriend";
+import { CardHeader, CardTitle } from "@/components/ui/card";
+import { Gamepad, Gamepad2 } from "lucide-react";
+import MatchmakingTitle from "./MatchmakingTitle";
+
+const GameMatchmaking = () => {
+	const [isCustom, setIscCustom] = useState(false);
+	const handleState = () => {
+		setIscCustom(!isCustom);
+	};
+	const [api, setApi] = useState<CarouselApi>();
+	const [current, setCurrent] = useState(0);
+
+	useEffect(() => {
+		if (!api) {
+			return;
+		}
+		setCurrent(api.selectedScrollSnap());
+
+		api.on("select", () => {
+			setCurrent(api.selectedScrollSnap());
+		});
+	}, [api]);
+	const { authenticatedUser } = useAuthentication();
+	const username = authenticatedUser!.user.username;
+	const { data, isLoading, isSuccess, isError, error } = useFriends(username);
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+
+	if (isError) {
+		return <p>Error: {error?.message}</p>;
+	}
+
+	if (isSuccess) {
+		console.log("Friends:", data);
+	}
+	return (
+		<div className="w-full h-full flex items-center justify-center">
+			<ResizablePanelGroup direction="horizontal">
+				<ResizablePanel className=" flex items-center justify-center">
+					<div className="h-full w-full flex flex-col items-center justify-center space-y-3">
+						<MatchmakingTitle />
+						<SelectMode handleState={handleState} />
+						{isCustom && <SelectModeColor />}
+						<GameSelectFriend />
+						<GameMatchmakingButtons />
+					</div>
+				</ResizablePanel>
+			</ResizablePanelGroup>
+		</div>
+	);
+};
+
+export default GameMatchmaking;
