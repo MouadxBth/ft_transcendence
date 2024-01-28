7252d214e8bdbc5be5ac23aa83fe7c36f518bf00
ENOLA | احسان <ihssane.ke@gmail.com>
2024-01-28 14:58:32 +0100

    Modified page.tsx
    removed Tabs compoenent

--- frontend/src/app/(sockets)/(authenticated)/game/page.tsx
+++ frontend/src/app/(sockets)/(authenticated)/game/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 import GameMatchmaking from "@/components/game/components/GameMatchmaking/GameMatchmaking";
-import { Tabs } from "@/components/ui/tabs";
 
 const Game = () => {
 	return (
 		<main className="flex justify-center items-center w-full h-full">
-			<Tabs
+			<div
 				defaultValue="auto matchmaking"
 				className="w-1/2 h-1/2 border flex flex-col items-center"
 			>
 				<GameMatchmaking />
-			</Tabs>
+			</div>
 		</main>
 	);
 };
