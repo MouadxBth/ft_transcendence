016f8144dc55686472e15f5a7f91ad6c27651006
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-29 02:56:36 +0100

    ~Modified ProfileInfoCard.tsx
    Switched to conditional rendering instead of hidden style

--- frontend/src/components/profile/info/ProfileInfoCard.tsx
+++ frontend/src/components/profile/info/ProfileInfoCard.tsx
@@ -2,11 +2,11 @@
 
 import { Card } from "@/components/ui/card";
 import { useAuthentication } from "@/hooks/authentication/useAuthentication";
-import { User } from "@/lib/types/user";
-import { backgroundStyles, cn } from "@/lib/utils";
+import { backgroundStyles } from "@/lib/utils";
 import ProfileInfoLevel from "./level/ProfileLevel";
 import ProfileInfo from "./ProfileInfo";
 import ProfileBlock from "./block/ProfileBlock";
+import { User } from "@/lib/types/user/user";
 
 export interface ProfileInfoCardProps {
 	user?: User;
@@ -18,17 +18,20 @@ const ProfileInfoCard = ({ user }: ProfileInfoCardProps) => {
 	const { nickname, username, firstName, lastName, avatar, banner, level, experience } =
 		currentUser;
 
+	const myProfile = username === authenticatedUser?.user.username;
+
 	return (
 		<Card
 			className="flex flex-col justify-center items-center py-2 w-full border-x-0 relative"
 			style={backgroundStyles(banner)}
 		>
-			<ProfileBlock
-				target={currentUser.username}
-				className={cn("absolute top-2 right-2 rounded-2xl", {
-					hidden: currentUser.username === authenticatedUser?.user.username,
-				})}
-			/>
+			{!myProfile && (
+				<ProfileBlock
+					target={currentUser.username}
+					className="absolute top-2 right-2 rounded-2xl"
+				/>
+			)}
+
 			<ProfileInfo
 				username={username}
 				nickname={nickname!}
