c828afe0ff5f5b152b3b3f080157a0532bdffa98
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-29 02:38:16 +0100

    ~Added useAchievementNotifications.tsx
    Added useAchievementNotifications hook to log info about achievements

---
+++ frontend/src/hooks/notifications/useAchievementNotifications.tsx
@@ -0,0 +1,62 @@
+import useSockets from "@/hooks/socket/useSockets";
+import { AchievementsUpdateType } from "@/lib/types/achievement/achievements-update";
+import { useEffect } from "react";
+import { useAuthentication } from "@/hooks/authentication/useAuthentication";
+import { toast as sonner } from "sonner";
+import { formattedDate } from "@/lib/utils";
+
+const AchievementData = ({
+	name,
+	unlockedAt,
+	brief,
+	description,
+}: {
+	name: string;
+	unlockedAt: string;
+	brief: string;
+	description: string;
+}) => {
+	return (
+		<div className="flex flex-col">
+			<div className="flex items-center space-x-2 pb-2">
+				<div className="font-bold">{name}</div>
+				<div className="text-xs">{formattedDate(unlockedAt)}</div>
+			</div>
+			<div className="text-xs">{brief}</div>
+			<div className="text-xs">{description}</div>
+		</div>
+	);
+};
+
+const useAchievementNotification = () => {
+	const { authenticatedUser } = useAuthentication();
+	const { notifications } = useSockets();
+	const myUsername = authenticatedUser?.user.username!;
+
+	useEffect(() => {
+		notifications?.on("achievement_awarded", (args: AchievementsUpdateType) => {
+			console.log("HERE");
+			if (myUsername !== args.username) return;
+
+			console.log("AGAIN HERE");
+
+			sonner.success("first");
+
+			sonner.success("Achievements", {
+				duration: 7 * 1e3,
+				dismissible: false,
+				important: true,
+				description: (
+					<AchievementData
+						name={args.latest.name}
+						unlockedAt={args.latest.unlockedAt}
+						brief={args.latest.brief ?? ""}
+						description={args.latest.description ?? ""}
+					/>
+				),
+			});
+		});
+	}, [notifications, myUsername]);
+};
+
+export default useAchievementNotification;
