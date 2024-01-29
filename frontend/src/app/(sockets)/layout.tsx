7f3b314b8fe6ed57d7a5094f4b7ed31154af7dd3
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-29 03:44:44 +0100

    ~Modified layout.tsx
    Fixed a recursion issue in SocketLayout

--- frontend/src/app/(sockets)/layout.tsx
+++ frontend/src/app/(sockets)/layout.tsx
@@ -3,19 +3,13 @@
 import useAchievementNotification from "@/hooks/notifications/useAchievementNotifications";
 import useErrorNotifications from "@/hooks/notifications/useErrorNotifications";
 import useFriendStatusNotifications from "@/hooks/notifications/useFriendStatusNotifications";
-import AuthenticationContextProvider from "@/providers/AuthenticationContextProvider";
-import SocketsContextProvider from "@/providers/SocketsContextProvider";
 import React from "react";
 
 const SocketLayout = ({ children }: { children: React.ReactNode }) => {
 	useErrorNotifications();
 	useFriendStatusNotifications();
 	useAchievementNotification();
-	return (
-		<AuthenticationContextProvider>
-			<SocketsContextProvider>{children}</SocketsContextProvider>
-		</AuthenticationContextProvider>
-	);
+	return <>{children}</>;
 };
 
 export default SocketLayout;
