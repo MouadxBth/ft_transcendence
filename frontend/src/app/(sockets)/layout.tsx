53fbd3e69f5a5d84f8cd2e966a3880beb79fd2e0
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2024-01-29 03:04:52 +0100

    ~Modified layout.tsx
    Modified SocketLayout to log notifications

--- frontend/src/app/(sockets)/layout.tsx
+++ frontend/src/app/(sockets)/layout.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import useAchievementNotification from "@/hooks/notifications/useAchievementNotifications";
+import useErrorNotifications from "@/hooks/notifications/useErrorNotifications";
+import useFriendStatusNotifications from "@/hooks/notifications/useFriendStatusNotifications";
+import AuthenticationContextProvider from "@/providers/AuthenticationContextProvider";
+import SocketsContextProvider from "@/providers/SocketsContextProvider";
 import React from "react";
 
 const SocketLayout = ({ children }: { children: React.ReactNode }) => {
-	return <>{children}</>;
+	useErrorNotifications();
+	useFriendStatusNotifications();
+	useAchievementNotification();
+	return (
+		<AuthenticationContextProvider>
+			<SocketsContextProvider>{children}</SocketsContextProvider>
+		</AuthenticationContextProvider>
+	);
 };
 
 export default SocketLayout;
