c73df461ab257724ec79abdd82cf8b35699799e1
zu-23 <h.zakaria52@gmail.com>
2024-01-31 00:28:54 +0100

    page file

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Pong from "./Pong";
+export default function Home() {
+
+  return (
+    <div>
+     <Pong />
+
+    </div>
+  );
+}
+
+// import React from 'react';
+// // import Pong from './Game';
+// import Pong from './class_game'
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div>
+//       <Pong />
+//     </div>
+//   );
+// }
+
+// export default App;
