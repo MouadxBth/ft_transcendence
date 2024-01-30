c664931204368c0d1248d44771b0385516308f4e
zu-23 <h.zakaria52@gmail.com>
2024-01-31 00:27:07 +0100

    button component file

---
+++ app/button.tsx
@@ -0,0 +1,33 @@
+
+// "use client";
+// import { useState } from "react";
+// import Pong from "./Pong";
+
+// function ChooseColor()
+// {
+//   let [comp, setComp] : any = useState(false);
+//   let [color, setColor]: any = useState("");
+//     return(
+//       <div>
+//         <button onClick={() =>{
+//           setColor("black");
+//           setComp(true);
+//         } }>black</button>
+//         <br />
+//         <button onClick={() => {
+//           setColor("green");
+//           setComp(true);
+//         }
+//       }>green</button>
+//         <br />
+//         <button onClick={() => {
+//           setColor("purple");
+//           setComp(true);
+//           }}>purple</button>
+//         {comp && <Pong color = {color}/>}
+//         <br />
+//       </div>
+//     );
+// }
+
+// export default ChooseColor;
