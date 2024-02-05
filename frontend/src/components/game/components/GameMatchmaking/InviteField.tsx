5b50c8bc13f68947ca06a786f3e41372dd2fb82f
Ihssane | احسان <ihssane.ke@gmail.com>
2024-02-05 18:34:04 +0100

    Refactor invite field in GameMatchmaking component

--- frontend/src/components/game/components/GameMatchmaking/InviteField.tsx
+++ frontend/src/components/game/components/GameMatchmaking/InviteField.tsx
@@ -1,22 +1,12 @@
 import { useEffect, useState } from "react";
 import useFriendsFetch from "@/hooks/user/friends/useFriendsFetch";
-
-import { Button } from "@/components/ui/button";
-import { FormField, FormItem } from "@/components/ui/form";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import {
-	Command,
-	CommandEmpty,
-	CommandGroup,
-	CommandInput,
-	CommandItem,
-} from "@/components/ui/command";
-import { Check, ChevronsUpDown } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FormItem } from "@/components/ui/form";
+import { Popover } from "@/components/ui/popover";
 import { FriendType } from "@/lib/types/friend/friend";
 import { Matchmakingprops } from "./GameMatchmaking";
 import { useAuthentication } from "@/hooks/authentication/useAuthentication";
+import SelectedFriend from "./SelectedFriend";
+import FriendsList from "./FriendsList";
 
 const InviteField = ({ form }: Matchmakingprops) => {
 	const { authenticatedUser } = useAuthentication();
@@ -46,87 +36,16 @@ const InviteField = ({ form }: Matchmakingprops) => {
 					) : (
 						<p className="text-xs text-muted-foreground">Leave empty for auto matchmaking</p>
 					)}
-					<PopoverTrigger asChild>
-						<Button
-							variant="outline"
-							role="combobox"
-							className="w-[300px] justify-between"
-						>
-							{value ? (
-								<div className="flex items-center w-full h-full space-x-2">
-									<Avatar className="items-center">
-										<AvatarImage
-											src={
-												userfriend?.avatar
-													? userfriend?.avatar.startsWith("http")
-														? userfriend?.avatar
-														: `${process.env.NEXT_PUBLIC_BACKEND_URL}/upload/avatar/${userfriend?.avatar}`
-													: ""
-											}
-											className="h-8 w-8 rounded-full object-cover"
-										/>
-										<AvatarFallback>
-											{userfriend?.nickname.toUpperCase().slice(0, 2)}
-										</AvatarFallback>
-									</Avatar>
-									<div>{userfriend?.nickname}</div>
-								</div>
-							) : (
-								<p>Invite a friend...</p>
-							)}
-							<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-						</Button>
-					</PopoverTrigger>
-					<PopoverContent className="w-[300px] p-0">
-						<Command>
-							<CommandInput placeholder="Search friend..." />
-							<CommandEmpty>No Friend with that nickname</CommandEmpty>
-							{data?.length !== 0 ? (
-								data?.map((friend) => (
-									<CommandGroup key={friend.username}>
-										<CommandItem
-											className="flex space-x-2"
-											value={friend.nickname}
-											onSelect={(currentValue) => {
-												setValue(currentValue === value ? "" : currentValue);
-												console.log(value);
-												console.log(currentValue);
-												form.setValue("invite", currentValue === value ? "" : currentValue);
-											}}
-										>
-											<Check
-												className={cn(
-													"mr-2 h-4 w-4",
-													value === friend.nickname ? "opacity-100" : "opacity-0"
-												)}
-											/>
-											<Avatar>
-												<AvatarImage
-													src={
-														friend.avatar
-															? friend.avatar.startsWith("http")
-																? friend.avatar
-																: `${process.env.NEXT_PUBLIC_BACKEND_URL}/upload/avatar/${friend.avatar}`
-															: ""
-													}
-													className="h-10 w-10 rounded-full object-cover"
-												/>
-												<AvatarFallback>{friend.nickname.toUpperCase().slice(0, 2)}</AvatarFallback>
-											</Avatar>
-											<div className="text-md">
-												{friend.nickname}
-												<div className="text-muted-foreground text-xs">
-													{friend.firstName} {friend.lastName}
-												</div>
-											</div>
-										</CommandItem>
-									</CommandGroup>
-								))
-							) : (
-								<p className="py-6 text-center text-sm">No Friend found. try automatchmaking</p>
-							)}
-						</Command>
-					</PopoverContent>
+					<SelectedFriend
+						value={value}
+						friend={userfriend}
+					/>
+					<FriendsList
+						form={form}
+						value={value}
+						data={friends}
+						setValue={setValue}
+					/>
 				</Popover>
 			</FormItem>
 		</>
