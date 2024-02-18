a537b2553554805a77a91a8dd3cf9ecf55ebf8c4
Ihssane | احسان <ihssane.ke@gmail.com>
2024-02-18 21:09:55 +0100

    Refactor conditional rendering in Game component

--- frontend/src/app/(sockets)/(authenticated)/game/page.tsx
+++ frontend/src/app/(sockets)/(authenticated)/game/page.tsx
@@ -7,16 +7,22 @@ import { useEffect, useState } from "react";
 import useSockets from "@/hooks/socket/useSockets";
 
 const Game = () => {
-	
 	const [status, setStatus] = useState("choose");
 	const [opponent, setOpponent] = useState("");
 
 	return (
 		<article className="flex justify-center items-center w-full h-full container flex-col space-y-3">
-				{
-					status === "choose" ? <GameMatchmaking setStatus={setStatus} setOpponent={setOpponent}/> :
-					<GameQueue setStatus={setStatus} opponent={opponent}/>
-				}
+			{status === "choose" ? (
+				<GameMatchmaking
+					setStatus={setStatus}
+					setOpponent={setOpponent}
+				/>
+			) : (
+				<GameQueue
+					setStatus={setStatus}
+					opponent={opponent}
+				/>
+			)}
 		</article>
 	);
 };
